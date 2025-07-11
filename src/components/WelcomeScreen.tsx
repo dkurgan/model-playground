9af1687169bb8d26e5e67fb68b49f99b036083ba
dkurgan <dunkanup@gmail.com>
2025-07-11 12:56:03 -0700

    icon fix

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -1,10 +1,12 @@
+import fireworksIcon from '/fireworks.svg'
+
 export const WelcomeScreen = () => {
   return (
     <div className="flex-1 flex flex-col items-center justify-center">
       <div className="text-center mb-16">
         <div className="flex items-center justify-center mb-8">
           <div className="w-6 h-6 mr-4">
-            <img src="/fireworks.svg" alt="Fireworks" className="w-full h-full" />
+            <img src={fireworksIcon} alt="Fireworks" className="w-full h-full" />
           </div>
           <h1 className="text-3xl font-normal text-gray-800">Welcome to Model Playground</h1>
         </div>
